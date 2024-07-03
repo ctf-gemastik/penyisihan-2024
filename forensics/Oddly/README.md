d9f6171f343c6ec6071126d4927deb03737c91c8
hanasuru <faqih.insani@ymail.com>
2024-07-04 02:19:55 +0700

    Add forensic/oddly

---
+++ forensics/Oddly/README.md
@@ -0,0 +1,23 @@
+# Oddly
+
+nagi
+
+---
+
+## Flag
+
+```
+gemastik{it_was_truly_odd_enough_to_craft_an_od_bash_shell_using_icmp}
+```
+
+## Description
+Someone stumbled upon peculiar service. There he found a customized ICMP server capable of executing shell commands. He then attempted to experiment with it while capturing the traffic. In the end, something happened, causing the traffic to become disordered
+
+## Difficulty
+easy
+
+## Tags
+pcap, shell
+
+## Notes
+intentionally left empty
---
+++ forensics/Oddly/writeup/sv.py
@@ -0,0 +1,36 @@
+from scapy.all import *
+from rod import ROD
+
+import zlib
+import re
+import os
+
+# Reorder packets based on timestamp order
+os.system('reordercap oddly.pcap oddly.pcapng')
+
+packets = rdpcap('oddly.pcapng')
+rule = re.compile(rb'dd if=/dev/loop1 bs=(\d+) skip=(\d+) count=(\d+)')
+
+# Allocate big buffer
+fd = open('storage.img', 'wb')
+fd.write(b'\x00' * int(7e+6))
+
+for enum, pkt in enumerate(packets):
+    icmp = pkt[ICMP]
+    data = zlib.decompress(pkt.load)
+
+    if icmp.type == 8:
+        if rule.search(data):
+            bs, skip, count = map(int, rule.findall(data)[0])
+    else:
+        data = data.decode()
+        data = ROD(data.splitlines()).ascii
+
+        if enum > 21:
+            fd.seek(bs*skip)
+            fd.write(data)
+
+# Exfiltrate each of flag.txt inside disk image
+os.system("echo bad6e0ff0b407254b5563fef9160c80a | sudo cryptsetup luksOpen storage.img storage")
+os.system("sudo fls /dev/mapper/storage | sort -t_ -k2 -n | grep -oP '\d+(?=:.f)' | xargs -l sudo icat /dev/mapper/storage $1 | paste -sd ''")
+os.system("sudo cryptsetup luksClose storage")
